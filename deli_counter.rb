06b45b55eb629a1bcd2304b63f554c8d152e3c71
proud-entropy-9548 <e.e.seieroe@gmail.com>
2018-11-12 19:16:40 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,9 +15,9 @@ def line(katz_deli)
  
 end
   
-def take_a_number(katz_deli, name)
+def take_a_number(katz_deli)
   katz_deli << name 
-  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+  puts "Welcome, you grabbed #{ticker_number}. You are number #{katz_deli.length} in line."
 end
 
 def now_serving(katz_deli) 
